ba42c680919612395d24590c3cccfde7f058f5e0
Dipendra <dipendrashrestha059@gmail.com>
2019-07-05 02:41:25 +0545

    initial

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import Simulation from '/js/Simulation.js';
 const WIDTH = 1200;
 const HEIGHT = 650;
 const FRAME_RATE = 60;
-const NUMBER_OF_BALLS = 50;
+const NUMBER_OF_BALLS = 10;
 const CONTAINER = document.getElementById('container');
 
 CONTAINER.style.width = appendPX(WIDTH);
